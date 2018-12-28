4ad2f6669f2ca3d4f42b18c2371aba832ba9a5e1
Austin Wise <AustinWise@gmail.com>
2018-12-28 01:08:11 -0800

    Add debugging settings and try it out.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let mut hello_str : &str = "Hello cruel world";
+    println!("{}", hello_str);
+    hello_str = "Hello borrow-checking world!";
+    println!("{}", hello_str);
 }
