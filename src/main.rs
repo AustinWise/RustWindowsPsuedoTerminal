88fc86e4bb26ce058d29644bff838932a07547a7
Austin Wise <AustinWise@gmail.com>
2018-12-28 03:26:20 -0800

    Test the APIs.
    The error handling could be nicer but this works for now.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate winapi;
+use std::ptr::null_mut;
 use winapi::shared::minwindef::DWORD;
-use winapi::um::winnt::{HANDLE, HRESULT};
+use winapi::um::handleapi::{CloseHandle, INVALID_HANDLE_VALUE};
+use winapi::um::namedpipeapi::CreatePipe;
 use winapi::um::wincon::COORD;
+use winapi::um::winnt::{HANDLE, HRESULT};
 
 type HPCON = *mut winapi::ctypes::c_void;
 extern "system" {
@@ -16,10 +19,52 @@ extern "system" {
     fn ClosePseudoConsole(hPC: HPCON);
 }
 
-fn main() {
+//TODO: build a real abstraction around pipes
+fn create_pipe_or_panic(read_pipe: &mut HANDLE, write_pipe: &mut HANDLE) {
     unsafe {
-        const ZERO_HPCON_VALUE: HPCON = 0isize as HPCON;
-        ClosePseudoConsole(ZERO_HPCON_VALUE);
+        let rc = CreatePipe(read_pipe, write_pipe, null_mut(), 0);
+        if rc != 1 {
+            panic!("CreatePipe: {}", rc);
+        }
+    }
+}
+
+fn close_pipe(pipe: &mut HANDLE) {
+    unsafe {
+        if *pipe != INVALID_HANDLE_VALUE {
+            CloseHandle(*pipe);
+            *pipe = INVALID_HANDLE_VALUE;
+        }
+    }
+}
+
+fn main() {
+    let size = COORD { X: 80, Y: 24 };
+    let mut pipe_pty_in = INVALID_HANDLE_VALUE;
+    let mut pipe_out = INVALID_HANDLE_VALUE;
+    create_pipe_or_panic(&mut pipe_pty_in, &mut pipe_out);
+    let mut pipe_in = INVALID_HANDLE_VALUE;
+    let mut pipe_pty_out = INVALID_HANDLE_VALUE;
+    create_pipe_or_panic(&mut pipe_in, &mut pipe_pty_out);
+    unsafe {
+        let mut hpc: HPCON = null_mut();
+        let rc = CreatePseudoConsole(size, pipe_pty_in, pipe_pty_out, 0, &mut hpc);
+        if rc != 0 {
+            panic!("CreatePseudoConsole: {}", rc);
+        }
+
+        close_pipe(&mut pipe_pty_in);
+        close_pipe(&mut pipe_pty_out);
+
+        let rc = ResizePseudoConsole(hpc, size);
+        if rc != 0 {
+            panic!("CreatePseudoConsole: {}", rc);
+        }
+
+        ClosePseudoConsole(hpc);
+
+        close_pipe(&mut pipe_in);
+        close_pipe(&mut pipe_out);
     }
 
     println!("done");
