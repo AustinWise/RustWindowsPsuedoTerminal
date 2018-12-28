8bed04b5f002c150489d4cf821387c470664a94d
Austin Wise <AustinWise@gmail.com>
2018-12-28 02:42:16 -0800

    Add defines.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
-
 extern crate winapi;
+use winapi::shared::minwindef::DWORD;
+use winapi::um::winnt::{HANDLE, HRESULT};
+use winapi::um::wincon::COORD;
 
 type HPCON = *mut winapi::ctypes::c_void;
 extern "system" {
-    fn ClosePseudoConsole(hPC : HPCON);
+    fn CreatePseudoConsole(
+        size: COORD,
+        hInput: HANDLE,
+        hOutput: HANDLE,
+        dwFlags: DWORD,
+        phPC: *mut HPCON,
+    ) -> HRESULT;
+    fn ResizePseudoConsole(hPC: HPCON, size: COORD) -> HRESULT;
+    fn ClosePseudoConsole(hPC: HPCON);
 }
 
 fn main() {
